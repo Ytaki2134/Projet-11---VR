8417b7cfbdd0605c7118c9645917af18fb4cdc10
Ytaki2134 <tdurand@gaming.tech>
2024-03-14 12:58:15 +0100

    Chest & Well Script

---
+++ Assets/Scripts/WellPuzzle.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class WellPuzzle : MonoBehaviour
+{
+    [SerializeField] private GameObject Reward;
+
+    private void OnTriggerEnter(UnityEngine.Collider other)
+    {
+        if (other.gameObject.tag == "Coin")
+        {
+        Debug.Log(other.gameObject.tag);
+            Instantiate(Reward, new Vector3(transform.position.x + 0.6f, transform.position.y + 1.2f, transform.position.z), Quaternion.Euler(new Vector3(-90f, -270f, 0f)));
+        }
+    }
+}
