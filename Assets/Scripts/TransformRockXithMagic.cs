b723cda3fdb7f8403d739c6ea7c0cd721e249b05
skaile <92392918+bielskialeksi@users.noreply.github.com>
2024-03-15 08:17:37 +0100

    Update script

---
+++ Assets/Scripts/TransformRockXithMagic.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformRockXithMagic : MonoBehaviour
+{
+
+    [SerializeField] private GameObject NormalRock;
+    [SerializeField] private GameObject FireRock;
+    [SerializeField] private GameObject IceRock;
+    [SerializeField] private GameObject ThunderRock;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+
+
+
+
+
+}
